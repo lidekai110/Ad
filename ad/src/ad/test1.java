8872e5d0c3c4219cb63ad90117e9accebe27189d
TEDU <TEDU@DESKTOP-IKGNEJ6>
2019-12-02 16:59:15 +0800

    werwer

---
+++ ad/src/ad/test1.java
@@ -0,0 +1,12 @@
+package ad;
+
+public class test1 {
+
+	private void mian() {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void main(String[] args) {
+		System.out.println("3.131313163");
+	}
+}
